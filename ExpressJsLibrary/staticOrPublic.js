64e5396f4bac85a697fafd3a13e5367a27929cbd
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-21 19:32:38 +0500

    updated...

--- ExpressJsLibrary/staticOrPublic.js
+++ ExpressJsLibrary/staticOrPublic.js
@@ -1,3 +1,16 @@
+
+app.use(express.static('static'))
+//this can be access easily without using the /static
+// /images/img.1
+// /style.css
+
+
+
+
+
+
+
+
 app.use('/static',express.static('static'))
 
 //css files or images can be access using the
