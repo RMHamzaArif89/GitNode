cd31570247d7616ed4f3e4f85304402250f57137
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-03-21 19:13:32 +0500

    static folder

--- ExpressJsLibrary/routes/staticOrPublic.js
+++ ExpressJsLibrary/routes/staticOrPublic.js
@@ -3,3 +3,7 @@ app.use('/static',express.static('static'))
 //css files or images can be access using the
 // /static/images/img.1
 // /static/style.css
+
+
+
+//files can by access from anywhere of folder
