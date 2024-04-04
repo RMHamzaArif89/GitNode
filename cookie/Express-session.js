79158714e85ecc86efe49e88e0174999f1851204
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-04-05 00:05:00 +0500

    created the express session

---
+++ cookie/Express-session.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const PORT = 4000;
+ 
+const app = express();
+ 
+// Initialization
+app.use(cookieParser());
+ 
+app.use(session({
+    secret: "RMHamza",
+    saveUninitialized: true,
+    resave: true,
+    // cookie: { secure: true }
+}));
+ 
+ 
+app.get('/', (req, res) => {
+    if (req.session.view) {
+ 
+        // The next time when user visits, 
+        // he is recognized by the cookie 
+        // and variable gets updated.
+        req.session.view++;
+        res.send("You visited this page for "
+            + req.session.view + " times");
+    }
+    else {
+ 
+        // If user visits the site for
+        // first time
+        req.session.view = 1;
+        res.send("You have visited this page"
+           + " for first time ! Welcome....");
+    }
+})
+ 
